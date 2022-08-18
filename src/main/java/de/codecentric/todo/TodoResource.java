88c14213c87ec504b36647e35000b92a945c71e0
Felix Rieß <felix.riess@codecentric.de>
2022-08-18 08:21:52 +0200

    mark todos as done

--- src/main/java/de/codecentric/todo/TodoResource.java
+++ src/main/java/de/codecentric/todo/TodoResource.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -23,6 +24,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Path("/todos")
+@Produces(MediaType.TEXT_HTML)
 public class TodoResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(TodoResource.class);
@@ -30,34 +32,49 @@ public class TodoResource {
 
     @CheckedTemplate
     public static class Templates {
-        public static native TemplateInstance todos(List<Todo> todoList);
-        public static native TemplateInstance todoDetail(Todo todo);
+        public static native TemplateInstance todos(final List<Todo> todoList);
+        public static native TemplateInstance todoDetail(final Todo todo);
     }
 
     @GET
-    @Produces(MediaType.TEXT_HTML)
     public TemplateInstance getTodoList() {
         return Templates.todos(this.todoList);
     }
 
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response addTodo(@MultipartForm TodoCreationForm todoCreationForm) {
+    public Response addTodo(@MultipartForm final TodoCreationForm todoCreationForm) {
         final Todo created = todoCreationForm.fromForm();
         this.todoList.add(created);
         LOG.info("Todo List elements {}", this.todoList);
-        return Response.seeOther(URI.create("/todos/" + created.getId().toString())).build();
+        return Response.seeOther(URI.create("/todos/")).build();
     }
 
     @GET
     @Path("/{todoId}")
-    @Produces(MediaType.TEXT_HTML)
-    public TemplateInstance getTodo(@PathParam("todoId")UUID todoId) {
-        Optional<Todo> todoWithId = this.todoList.stream().filter(todo -> todo.getId().equals(todoId)).findFirst();
+    public TemplateInstance getTodo(@PathParam("todoId") final UUID todoId) {
+        Optional<Todo> todoWithId = findById(todoId);
         if (todoWithId.isPresent()) {
             return Templates.todoDetail(todoWithId.get());
         } else {
             throw new NotFoundException("Todo with id " + todoId.toString() + " is not existing");
         }
+    }
+
+    @POST
+    @Path("/{todoId}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response markTodoAsDone(@PathParam("todoId") final UUID todoId) {
+        Optional<Todo> todoWithId = findById(todoId);
+        if (todoWithId.isPresent()) {
+            todoWithId.get().markComplete();
+            return Response.seeOther(URI.create("/todos/")).build();
+        } else {
+            throw new NotFoundException("Todo with id " + todoId.toString() + " is not existing");
+        }
+    }
+
+    private Optional<Todo> findById(final UUID id) {
+        return this.todoList.stream().filter(todo -> todo.getId().equals(id)).findFirst();
     }
 }
