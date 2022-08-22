9df715dee9497596cf18ee7067f091f4a9bb73e1
Felix Rieß <felix.riess@codecentric.de>
2022-08-22 11:03:13 +0200

    use resteasy annotation

--- src/main/java/de/codecentric/todo/rest/TodoCreationForm.java
+++ src/main/java/de/codecentric/todo/rest/TodoCreationForm.java
@@ -1,13 +1,13 @@
 package de.codecentric.todo.rest;
 
 import org.jboss.resteasy.reactive.PartType;
+import org.jboss.resteasy.reactive.RestForm;
 
-import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 
 public class TodoCreationForm {
 
-    private @FormParam("name") @PartType(MediaType.TEXT_PLAIN) String name;
+    private @RestForm("name") @PartType(MediaType.TEXT_PLAIN) String name;
 
     public String getName() {
         return name;
